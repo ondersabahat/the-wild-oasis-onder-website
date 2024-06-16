3c61ab26ed161fcc787b79e30f850d0fe3175c7e
ondersabahat <ondersabahat@gmail.com>
2024-06-16 15:43:28 +0300

    Initial commit

---
+++ app/cabins/[cabinId]/page.js
@@ -0,0 +1,58 @@
+import Cabin from "@/app/_components/Cabin";
+import Reservation from "@/app/_components/Reservation";
+import Spinner from "@/app/_components/Spinner";
+import {
+  getBookedDatesByCabinId,
+  getCabin,
+  getCabins,
+  getSettings,
+} from "@/app/_lib/data-service";
+import { Suspense } from "react";
+
+export async function generateMetadata({ params }) {
+  const { cabinId } = params;
+  const cabin = await getCabin(cabinId);
+  const { name } = cabin;
+
+  return { title: `Cabin ${name}` };
+}
+
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+
+  return ids;
+}
+
+export default async function Page({ params }) {
+  const { cabinId } = params;
+
+  const cabin = await getCabin(cabinId);
+
+  const settings = await getSettings();
+
+  const bookedDates = await getBookedDatesByCabinId(cabinId);
+
+  // const [cabin, settings, bookedDates] = await Promise.all([
+  //   getCabin(cabinId),
+  //   getSettings(),
+  //   getBookedDatesByCabinId(cabinId),
+  // ]);
+
+  return (
+    <div className="max-w-6xl mx-auto mt-8">
+      <Cabin cabin={cabin} />
+
+      <div>
+        <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
+          Reserve {cabin.name} today. Pay on arrival.
+        </h2>
+
+        <Suspense fallback={<Spinner />}>
+          <Reservation cabin={cabin} />
+        </Suspense>
+      </div>
+    </div>
+  );
+}
